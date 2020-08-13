a8c5348cc8e4e513595353c9178fc262340e4d3c
18041430 <18041430@myrp.edu.sg>
2020-08-13 12:58:17 +0800

    Done

---
+++ app/src/main/java/sg/edu/rp/webservices/demoandroidwear/ReplyActivity.java
@@ -0,0 +1,32 @@
+package sg.edu.rp.webservices.demoandroidwear;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.RemoteInput;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class ReplyActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reply);
+
+        CharSequence reply = null;
+        Intent intnet = getIntent();
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intnet);
+        if (remoteInput != null){
+            reply = remoteInput.getCharSequence("status");
+        }
+
+        if (reply != null){
+            Toast.makeText(ReplyActivity.this, "You have indicated: " + reply, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
